afb1219083b9b5365b09915e024bcff1fe8c0146
Tejashwa2810 <tejashwa2810@gmail.com>
2025-06-01 01:33:39 +0530

    Added Redis functionality completely

---
+++ src/main/java/com/example/redistest/Repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.redistest.Repository;
+
+import com.example.redistest.Models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
